4c9a5c86ec2373a5b1560c2abd8bff1657b6f925
Jörn Berkefeld <joern.berkefeld@gmail.com>
2026-04-10 15:25:30 +0200

    chore: exclude .vscode-test from prettier+eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ export default tseslint.config(
             'server/out/**',
             '**/*.vsix',
             '**/coverage/**',
+            '.vscode-test/**',
         ],
     },
     eslint.configs.recommended,
